ca91379fa2acaf9185218e3ff5293e81b65d5a27
Pekka Alaluukas <pekka.alaluukas@hotmail.com>
2022-11-21 09:49:57 +0200

    sovellus alustettu

---
+++ __test__/app.test.js
@@ -0,0 +1,6 @@
+const calculator = require('../calculator.js')
+
+test('Funktion tulisi palauttaa sille annetun kahden kokonaisluvun summa', () => {
+    expect(calculator.add(4,5)).toBe(9);
+    expect(calculator.add(4,-5)).toBe(-1);
+  });
